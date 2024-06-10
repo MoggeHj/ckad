bf7ed653256601d864d23e5f75afcb13955ca74d
Morgan Hjelm <morgan.helm82@gmail.com>
2024-06-10 14:05:30 +0200

    Ckad; Application Deployment

--- 3 Application Observability and Maintenance/3 Implementing Probes and Health Checks/health-probes/obj/Debug/net7.0/health-probes.AssemblyInfo.cs
+++ 3 Application Observability and Maintenance/3 Implementing Probes and Health Checks/health-probes/obj/Debug/net7.0/health-probes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece3686d758800c1c4a69ea6b9647bf50e9401b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("health-probes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
